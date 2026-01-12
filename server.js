f20f789497959e59b401ba6a11a541a7c001892b
Amanda Ogletree <amandaogletree19@gmail.com>
2026-01-12 16:15:39 -0500

    Updated GET with Facade pattern (transformed data) to specifically display text value of recieved data to users in new object with fact property.

--- server.js
+++ server.js
@@ -12,16 +12,21 @@ app.get('/api/fun-fact', async (req, res)=> {
     try{
         const response = await apiClient.get('facts/random')
         
-        res.json(response.data)
+        const fact = response.data
+        const transformedFact = {
+            "fact": fact.text
+        }
+
+        res.json(transformedFact)
 
     }catch (error){
         //Status Erros
         if(error.response){
             console.error('API Error: ', error.response.status, error.response.data) //for devs
-            res.status.apply(error.response.status).json({error: 'Error fetching data from the API. (⊙_⊙)？'}) //for users
+            res.status.apply(error.response.status).json({error: 'Could not fetch fun fact from API. (っ °Д °;)っ'}) //for users
         }else{ //Network Errors
             console.error('Network Error: ', error.message)
-            res.status(500).json({error: 'A network error occured.(´。＿。｀)'})
+            res.status(500).json({error: 'A network error occured while fetching your fact.(´。＿。｀)'})
         }
     }
 })
